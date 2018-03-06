85cff8d3799ef108e1a3ed3545357991ce85d8ff
Ahmed Habib <ahmed.habib@hbluk.com>
2018-03-06 13:17:48 +0000

    Convert to MVC design pattern.

---
+++ Todoey/DataModel/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Ahmed Habib on 06/03/2018.
+//  Copyright © 2018 AhmedHabib. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title = ""
+    var done = false
+}
